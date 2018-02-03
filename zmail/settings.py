04c9559965a292aa0aee8a3fac2b59299c157160
ZYunH <workvl@163.com>
2018-02-03 23:57:49 +0800

    [Fix]Add local hostname to avoid server reject mail in windows10

--- zmail/settings.py
+++ zmail/settings.py
@@ -3,9 +3,10 @@ zmail.settings
 ~~~~~~~~~~~~
 This module contains some settings for zmail running or test.
 """
-import logging
 
 __status__ = 'publish'
 
 # Logging level: INFO == 20 , WARNING == 30 , ERROR == 40 , CRITICAL == 50
-__level__ = logging.INFO if __status__ == 'dev' else logging.WARNING
+__level__ = 20 if __status__ == 'dev' else 30
+
+__local__ = 'zmail.local'

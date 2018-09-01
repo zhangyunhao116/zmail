91d06d7b4bb79ab75bbc12269b30612b7bf3d308
ZYunH <zyunhjob@163.com>
2018-09-01 23:57:09 +0800

    Improve server structure

--- zmail/abc.py
+++ zmail/abc.py
@@ -29,16 +29,44 @@ class ProtocolServer(ABC):
             raise TypeError('Can not use ssl and tls together.')
 
     @abstractmethod
-    def login(self):
+    def _make_server(self):
         pass
+
+    def _remove_server(self):
+        self.server = None
+
+    @abstractmethod
+    def login(self):
+        if self._login:
+            self.log_exception('{} duplicate login!'.format(self.__repr__()))
+            return
+
+        if self.debug:
+            self.log_access('login')
+
+        self._make_server()
+
+        if self.tls:
+            self.stls()
+
+        # Not implement.
 
     @abstractmethod
     def logout(self):
         pass
 
     @abstractmethod
-    def check_available(self) -> bool:
+    def stls(self):
         pass
+
+    def check_available(self) -> bool:
+        try:
+            self.login()
+            self.logout()
+            return True
+        except Exception as e:
+            self.log_exception('{} access error :{}'.format(self.__class__.__name__, e))
+            return False
 
     def is_login(self) -> bool:
         return self._login
@@ -48,6 +76,20 @@ class ProtocolServer(ABC):
 
     def log_exception(self, msg):
         self.log.fatal(msg)
+
+    def log_access(self, msg):
+        self.log_debug('<{} {}:{} ssl:{} tls:{} is_login:{}> {}.'
+                       .format(self.__class__.__name__,
+                               self.host, self.port,
+                               self.ssl, self.tls,
+                               self.is_login(), msg))
+
+    def __enter__(self):
+        self.login()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.logout()
 
     def __repr__(self):
         return '<{} username:{} password:{} ' \

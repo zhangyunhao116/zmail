9d58502b0f022a2f18345c92fe967270b45aab55
ZYunH <zyunhjob@163.com>
2018-10-10 19:17:41 +0800

    Remove unused func

--- zmail/utils.py
+++ zmail/utils.py
@@ -8,18 +8,6 @@ import os
 import sys
 
 
-def bytes_to_string(bytes_list):
-    """Decode a list of bytes-objects to a list of string-objects."""
-    return tuple(map(lambda x: x.decode(), bytes_list))
-
-
-def type_check(_types, *args):
-    """Each args must be a type in _types or raise a TypeError."""
-    for obj in args:
-        if not isinstance(obj, _types):
-            raise TypeError('Parameter {} must be {}'.format(obj, _types))
-
-
 def get_abs_path(file):
     """if the file exists, return its abspath or raise a exception."""
     work_path = os.path.abspath(os.getcwd())

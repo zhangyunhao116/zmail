9f6050be9f978b293ce057609a6e5779cbacf2cc
ZYunH <zyunhjob@163.com>
2018-12-23 15:17:43 +0800

    Now use 'application/octet-stream' attachment

--- zmail/mime.py
+++ zmail/mime.py
@@ -1,12 +1,9 @@
 import logging
-import mimetypes
 import os
 import warnings
 from email.encoders import encode_base64
 from email.header import Header
-from email.mime.audio import MIMEAudio
 from email.mime.base import MIMEBase
-from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from typing import List, Optional
@@ -128,31 +125,14 @@ class Mail:
 def make_attachment_part(file_path) -> MIMEBase:
     """According to file-type return a prepared attachment part."""
     name = os.path.split(file_path)[1]
-    file_type = mimetypes.guess_type(name)[0]
-
     encoded_name = Header(name).encode()
 
-    if file_type is None:
-        logger.warning('Could not guess %s type, use application type instead.', file_path)
-        file_type = 'application/octet-stream'
-
+    file_type = 'application/octet-stream'
     main_type, sub_type = file_type.split('/')
 
-    if main_type == 'text':
-        with open(file_path, 'r') as f:
-            part = MIMEText(f.read())
-            part['Content-Disposition'] = 'attachment;filename="{}"'.format(encoded_name)
-
-    elif main_type in ('image', 'audio'):
-        with open(file_path, 'rb') as f:
-            part = MIMEImage(f.read(), _subtype=sub_type) if main_type == 'image' else \
-                MIMEAudio(f.read(), _subtype=sub_type)
-            part['Content-Disposition'] = 'attachment;filename="{}"'.format(encoded_name)
-
-    else:
-        with open(file_path, 'rb') as f:
-            part = MIMEBase(main_type, sub_type)
-            part.set_payload(f.read())
-            part['Content-Disposition'] = 'attachment;filename="{}"'.format(encoded_name)
-            encode_base64(part)
+    with open(file_path, 'rb') as f:
+        part = MIMEBase(main_type, sub_type)
+        part.set_payload(f.read())
+        part['Content-Disposition'] = 'attachment;filename="{}"'.format(encoded_name)
+        encode_base64(part)
     return part
